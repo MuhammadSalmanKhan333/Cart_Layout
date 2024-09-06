3bd574404ea6f8c8cfcfb8e01e302ca1e8903a7f
MuhammadSalmanKhan333 <salmanmkhan2002@gmail.com>
2024-09-06 13:34:15 +0500

    Discount Functionality done

--- AddToCart_layout/index.js
+++ AddToCart_layout/index.js
@@ -1,3 +1,7 @@
+const discPercentage = 25;
+// let disc = document.getElementById("discount");
+let disc = document.getElementsByClassName("discount");
+disc[0].textContent = `-${discPercentage}%`;
 const thumbnails = document.querySelectorAll(".thumbnail");
 const mainImage = document.getElementById("main-image");
 
@@ -17,3 +21,36 @@ thumbnails.forEach((khan) => {
     khan.classList.add("active");
   });
 });
+
+const priceObj = {
+  S: 132.0,
+  M: 135.0,
+  L: 138.0,
+  XL: 145.0,
+};
+
+let originalEle = document.getElementById("original-price");
+let discountEle = document.getElementById("discounted-price");
+const sizeButtons = document.querySelectorAll(".size-selector button");
+
+var bttn = document.getElementById("addToBag");
+bttn.style.display = "none";
+changeSize("M");
+
+function changeSize(selectedSize) {
+  bttn.style.display = "block";
+  var btn = document.getElementById(selectedSize);
+  sizeButtons.forEach((btn) => {
+    btn.classList.remove("selected");
+  });
+  btn.classList.add("selected");
+  originalEle.textContent = priceObj[selectedSize].toFixed(2);
+  calculatePrice(priceObj[selectedSize]);
+}
+
+function calculatePrice(price) {
+  let discountPrice = discPercentage / 100;
+  let finalPrice = (price - price * discountPrice).toFixed(2);
+  console.log(finalPrice);
+  discountEle.textContent = `$${finalPrice}`;
+}
